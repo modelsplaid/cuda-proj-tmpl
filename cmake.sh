216750a1c652f928a581becddae447c94b695208
modelsplaid <modelsplaid@11.com>
2024-05-30 11:32:44 +0800

    cutlass import works. todo: learn cmake

---
+++ cmake.sh
@@ -0,0 +1,5 @@
+rm -rf build
+mkdir build
+cd build
+cmake -DCMAKE_CUDA_ARCHITECTURES="80"   ..
+#make -j40
--- cmpil.sh
+++
@@ -1,6 +0,0 @@
-#rm -rf build
-#mkdir build
-
-cd build
-cmake -DCMAKE_CUDA_ARCHITECTURES="80" -DCUTLASS_NVCC_ARCHS=80  ..
-make -j40
