08944b61b52e83225ca51a43df6994be315d0d1c
modelsplaid <modelsplaid@11.com>
2024-06-04 13:28:37 +0800

    moved cutlass example to here

--- cmake_only.sh
+++ cmake_only.sh
@@ -1,5 +1,8 @@
 rm -rf build
 mkdir build
 cd build
-cmake -DCMAKE_CUDA_ARCHITECTURES="80"   ..
+#cmake -DCMAKE_CUDA_ARCHITECTURES="80"   ..
+
+cmake -DCMAKE_CUDA_ARCHITECTURES="80" -DCMAKE_PREFIX_PATH="$(python3 -c 'import torch.utils; print(torch.utils.cmake_prefix_path)')" ../
+make
 #make -j40
--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-./build/src/saxpy_cuda
+#./build/src/saxpy_cuda
+
+echo "---bach sz == 16"
+#./build/src/saxpy_cuda  --head_number=32 --batch_size=16 --head_size=128 --head_size_v=64 --seq_length=1 --seq_length_kv=768  --iterations=1 --fixed_seq_length=true --causal=false
+#./build/src/saxpy_cuda  --head_number=32 --batch_size=16 --head_size=128 --head_size_v=64 --seq_length=1 --seq_length_kv=1  --iterations=1 --fixed_seq_length=true --causal=false
+./build/src/saxpy_cuda  --head_number=32 --batch_size=16 --head_size=128 --head_size_v=64 --seq_length=1 --seq_length_kv=769  --iterations=1 --fixed_seq_length=true --causal=false
+
